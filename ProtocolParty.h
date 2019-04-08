cc5c18852525d28a8b8bbbf8cef6502342ee84e7
levymeit <levymeit@gmail.com>
2019-04-08 09:56:41 +0000

    dividematrix multiplication into 2 calls

--- ProtocolParty.h
+++ ProtocolParty.h
@@ -3001,8 +3001,8 @@ int ProtocolParty<FieldType>::generateSharedMatricesForGPU(vector<FieldType> &sh
     int num_devices = 1;
    cudaSafeCall(cudaGetDeviceCount(&num_devices));
     printf("%d devices used\n", num_devices);
-    std::vector<int> devices(num_devices*threads_per_device);
-    for (int device = 0; device < num_devices; ++device)
+    std::vector<int> devices((num_devices-1)*threads_per_device);
+    for (int device = 1; device < num_devices; ++device)
     {
         for (int i = 0; i < threads_per_device; ++i){
             devices[threads_per_device*device +i] = device;
