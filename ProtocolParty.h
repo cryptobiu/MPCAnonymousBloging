5c405d1ef69729dc10a344f43c0b624f21be72a4
Meital Levy <levymeit@gmail.com>
2019-04-16 09:41:20 +0300

    generalizing gpu threads use

--- ProtocolParty.h
+++ ProtocolParty.h
@@ -221,6 +221,8 @@ public:
     int validMsgsTestFlat(vector<FieldType> &msgsVectors, vector<FieldType> &msgsVectorsSquares, vector<FieldType> & counters, vector<FieldType> &unitVectors);
     int unitVectorsTestFlat(vector<FieldType> &vecs, int size, FieldType *randomElements, vector<FieldType> &sumsForConsistensyTest, bool toSplit);
     void processSums(FieldType* sum, FieldType* constRandomBits, int size, FieldType* vecs, int device);
+
+    processSumsThreads(int start, int end, int deviceID);
 //    int unitVectorsTest(vector<vector<FieldType>> &vecs, FieldType *randomElements,vector<FieldType> &sumsForConsistensyTest);
     int unitWith1VectorsTest(vector<vector<FieldType>> &vecs);
 
@@ -2345,31 +2347,31 @@ cout<<"--------- reg result ----------------------------"<<endl;
         int num_devices = 1;
         cudaSafeCall(cudaGetDeviceCount(&num_devices));
         printf("%d devices used\n", num_devices);
-        std::vector<int> devices((num_devices)*threads_per_device);
-        for (int device = 0; device < num_devices ; ++device)
-        {
-            for (int i = 0; i < threads_per_device; ++i){
-                devices[threads_per_device*device +i] = device;
-                cout<<"vec is "<<device<<endl;
-            }
+
+
+        int sizeForEachThread;
+        if (8 <= numThreads){
+            numThreads = 8;
+            sizeForEachThread = 1;
+        } else{
+            sizeForEachThread = (8 + numThreads - 1)/ numThreads;
         }
 
         vector<thread> threadsForGPU(devices.size());
+
         for (int i = 0; i < num_devices; i++) {
-            threads[i] = thread(&ProtocolParty::processSums, this, sum1.data() + sum1.size() * i / 8,
-                                constRandomBitsFor1.data(), size,
-                                vecs.data() + vecs.size() * i / 8,
-                                devices[i]);
-        }
-        for (int i = 0; i < num_devices; i++) {
-            threads[num_devices + i] = thread(&ProtocolParty::processSums, this, sum0.data() + sum0.size() * i / 8,
-                                              constRandomBitsFor0.data(), size,
-                                              vecs.data() + vecs.size() * i / 8,
-                                              devices[8 + i]);
+            if ((t + 1) * sizeForEachThread <= 8) {
+                threadsForGPU[i] = thread(&ProtocolParty::processSumsThreads, this, t * sizeForEachThread,
+                                          (t + 1) * sizeForEachThread, i);
+            }
+            else{
+                threadsForGPU[i] = thread(&ProtocolParty::processSumsThreads, this, t * sizeForEachThread,
+                                          8, i);
+            }
         }
 
-        for (int t = 0; t < 16; t++) {
-            threads[t].join();
+        for (int t = 0; t < num_devices; t++) {
+            threadsForGPU[t].join();
         }
     } else {
         processSums(sum1.data(), constRandomBitsFor1.data(), size,  vecs.data(), 0);
@@ -2478,6 +2480,27 @@ void ProtocolParty<FieldType>::processSums(FieldType* sum, FieldType* constRando
     processNN31((merssene31_t *) sum, (merssene31_t *) constRandomBits, size, securityParamter,
                     (merssene31_t *) vecs, batchSize / 8, size, device);
 
+}
+
+
+template <class FieldType>
+void ProtocolParty<FieldType>::processSumsThreads(int start, int end, int deviceID){
+
+
+    for(int i=start, i<end; i++){
+
+
+    processNN31((merssene31_t *)sum1.data() + sum1.size()*i/8,
+                (merssene31_t *)constRandomBitsFor1.data(), size, securityParamter,
+                (merssene31_t *)vecs.data() + vecs.size()*i/8, batchSize/8, size,
+                deviceID);
+
+    processNN31((merssene31_t *)sum0.data() + sum0.size()*i/8,
+                (merssene31_t *)constRandomBitsFor0.data(), size, securityParamter,
+                (merssene31_t *)vecs.data() + vecs.size()*i/8, batchSize/8, size,
+                deviceID);
+
+    }
 }
 
 
