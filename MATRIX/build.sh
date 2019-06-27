b8e7d04181986f22e482fcc13ada0b884fb671f0
liorbiu <liork.cryptobiu@gmail.com>
2019-06-27 11:48:04 +0300

    MATRIX scripts for cpu

---
+++ MATRIX/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ../MPCAnonymousBloggingClient && cmake . && make
+
+cd ../ && cmake . && make
---
+++ MATRIX/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
+   -fieldType ZpMersenne31 -l 40
+
+cd ../MPCAnonymousBlogging &&./MPCAnonymousBloging -partyID ${1} -numServers ${2} -numClients ${3} \
+   -fieldType ZpMersenne31 -l 40 -partiesFile parties.conf  -internalIterationsNumber 1 -numThreads ${4}
