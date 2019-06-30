2914a770f0c91d55af5aa40ba0b84626ef7dba11
liorbiu <liork.cryptobiu@gmail.com>
2019-06-30 16:09:32 +0300

    Delete old clients files before generating new

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $4 = "1" ]; then
+rm -rf ~/files*
 cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l 40
 fi
