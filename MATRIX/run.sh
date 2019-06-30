1bc2a31f3a16fb7429221810721c2d85a0b15ea6
liorbiu <liork.cryptobiu@gmail.com>
2019-06-30 15:58:07 +0300

    Clients generated only when numThreads=1

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+if [ $4 = "1" ]; then
 cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l 40
+fi
 
 cd ../ && ./MPCAnonymousBloging -partyID ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l 40 -partiesFile parties.conf  -internalIterationsNumber 1 -numThreads ${4}
