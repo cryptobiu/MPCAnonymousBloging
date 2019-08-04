323a6eb3a40f6586853a0ebd29f3098a3d853ec2
Lior Koskas <lior121987@gmail.com>
2019-08-04 15:43:28 +0300

    Fix paths

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -3,5 +3,5 @@
 cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l ${4}
 
-cd ../MPCAnonymousBlogging && ./MPCAnonymousBloging -partyID ${1} -numServers ${2} -numClients ${3} \
+cd ../ && ./MPCAnonymousBloging -partyID ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l ${4} -partiesFile parties.conf  -internalIterationsNumber 1 -numThreads ${5}
