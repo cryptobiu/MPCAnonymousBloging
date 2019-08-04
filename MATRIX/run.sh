3914427e2e1aa5430e07c63abd8735fa676a5e8a
Lior Koskas <liork.cryptobiu@gmail.com>
2019-08-04 14:03:09 +0300

    Update run.sh

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
-   -fieldType ZpMersenne31 -l 40
+   -fieldType ZpMersenne31 -l ${4}
 
 cd ../MPCAnonymousBlogging && ./MPCAnonymousBloging -partyID ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l ${4} -partiesFile parties.conf  -internalIterationsNumber 1 -numThreads ${5}
