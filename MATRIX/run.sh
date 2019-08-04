b726f4588a7d8591636d7f0dd4f5f85c736d10c3
Lior Koskas <liork.cryptobiu@gmail.com>
2019-08-04 14:03:01 +0300

    Update run.sh

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -3,7 +3,7 @@
 if [ $4 = "1" ]; then
 rm -rf ~/files*
 cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
-   -fieldType ZpMersenne31 -l 40
+   -fieldType ZpMersenne31 -l ${4}
 fi
 
 cd ../ && ./MPCAnonymousBloging -partyID ${1} -numServers ${2} -numClients ${3} \
