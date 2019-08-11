eec14c57eca136165c2da904a50c8b31a89f9f91
liorbiu <liork.cryptobiu@gmail.com>
2019-08-11 13:06:39 +0300

    Creating files is optional

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+if [ $6 = "true" ]; then
+rm -rf ~/files
 cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l ${4}
+fi
 
 cd ../ && ./MPCAnonymousBloging -partyID ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l ${4} -partiesFile parties.conf  -internalIterationsNumber 1 -numThreads ${5}
