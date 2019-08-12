b9a7f5bb7a5e8f934134f213b8a17ae5ed183e57
liorbiu <liork.cryptobiu@gmail.com>
2019-08-12 10:32:23 +0300

    Fix files dir path

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $6 = "true" ]; then
-rm -rf ~/files
+rm -rf "$HOME/files${3}"
 cd ../MPCAnonymousBloggingClient && ./MPCAnonymousBloggingClient -server ${1} -numServers ${2} -numClients ${3} \
    -fieldType ZpMersenne31 -l ${4}
 fi
