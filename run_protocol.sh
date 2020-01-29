ea64d047b5d4cd3f6af000a40bc03aa050a0ac30
Meital Levy <levymeit@gmail.com>
2020-01-29 10:47:10 +0200

    adding a simulation mode

--- run_protocol.sh
+++ run_protocol.sh
@@ -2,6 +2,6 @@
 for i in `seq "${1}" 1 "${2}"`;
 do
 	./MPCAnonymousBloging  -l 40 -partyID "${i}" -numServers "${3}" -numClients "${4}" -fieldType ZpMersenne31 \
-	 -partiesFile Parties.txt  -internalIterationsNumber 1 -numThreads "${5}" -T "${6}" -toUmount "${7}" &
+	 -partiesFile Parties.txt  -internalIterationsNumber 1 -numThreads "${5}" -T "${6}" -toUmount false  -toSimulate "${7}" &
 	echo "Running $i..."
 done
