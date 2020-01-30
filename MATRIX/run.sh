d1c325c5de04ed825806ae74c946b9fa5104de68
Lior Koskas <lior121987@gmail.com>
2020-01-30 11:42:10 +0200

    Add simulation flag

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -8,4 +8,4 @@ fi
 
 ./MPCAnonymousBloging -partyID "${1}" -numServers "${2}" -numClients "${3}" \
    -fieldType ZpMersenne31 -l "${4}" -partiesFile "${5}" -internalIterationsNumber "${6}" -numThreads "${7}" \
-    -T "${8}" -toUmount "${11}"
+    -T "${8}" -toUmount "${11}" -toSimulate "${12}"
