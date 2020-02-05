9b81b07f1c3345a35262cdc7c770660f3c7c8c31
Lior Koskas <lior121987@gmail.com>
2020-02-05 14:05:41 +0200

    Remove paramater

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -8,4 +8,4 @@ fi
 
 ./MPCAnonymousBloging -partyID "${1}" -numServers "${2}" -numClients "${3}" \
    -fieldType ZpMersenne31 -l "${4}" -partiesFile "${5}" -internalIterationsNumber "${6}" -numThreads "${7}" \
-    -T "${8}" -toUmount "${11}" -toSimulate "${12}"
+    -T "${8}" -toUmount "${10}" -toSimulate "${11}"
