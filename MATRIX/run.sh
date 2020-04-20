832cdd25b7c23a3688d5c1ca5b711ec565685f44
liorko87 <lior121987@gmail.com>
2020-04-20 15:43:39 +0300

    Add partiesNumber param

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -6,6 +6,6 @@ sudo rm -rf ~/files${3}*
    -fieldType ZpMersenne31 -l "${4}" -T "${8}" -toMount "${10}"
 fi
 
-./MPCAnonymousBloging -partyID "${1}" -numServers "${2}" -numClients "${3}" \
+./MPCAnonymousBloging -partyID "${1}" -numServers "${2}" -numClients "${3}" -partiesNumber "${3}" \
    -fieldType ZpMersenne31 -l "${4}" -partiesFile "${5}" -internalIterationsNumber "${6}" -numThreads "${7}" \
     -T "${8}" -toUmount "${10}" -toSimulate "${11}"
